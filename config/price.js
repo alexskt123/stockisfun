7e330eef3e26f9d58fdf5479c52a729a1b379665
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-02-02 23:35:15 +0800

    make code better

---
+++ config/price.js
@@ -0,0 +1,66 @@
+export const chartDataSet = {
+    fill: false,
+    lineTension: 0.1,
+    borderCapStyle: 'butt',
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: 'miter',
+    pointBackgroundColor: '#fff',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10,
+}
+
+export const dateRange = [
+    {
+        'fromDate': '2021-01-01',
+        'toDate': '2021-12-31'
+    },
+    {
+        'fromDate': '2020-01-01',
+        'toDate': '2020-12-31'
+    },
+    {
+        'fromDate': '2019-01-01',
+        'toDate': '2019-12-31'
+    },
+    {
+        'fromDate': '2018-01-01',
+        'toDate': '2018-12-31'
+    },
+    {
+        'fromDate': '2017-01-01',
+        'toDate': '2017-12-31'
+    },
+    {
+        'fromDate': '2016-01-01',
+        'toDate': '2016-12-31'
+    },
+    {
+        'fromDate': '2015-01-01',
+        'toDate': '2015-12-31'
+    },
+    {
+        'fromDate': '2014-01-01',
+        'toDate': '2014-12-31'
+    },
+    {
+        'fromDate': '2013-01-01',
+        'toDate': '2013-12-31'
+    },
+    {
+        'fromDate': '2012-01-01',
+        'toDate': '2012-12-31'
+    },
+    {
+        'fromDate': '2011-01-01',
+        'toDate': '2011-12-31'
+    },
+    {
+        'fromDate': '2010-01-01',
+        'toDate': '2010-12-31'
+    },
+]
--- pages/price.js
+++ pages/price.js
@@ -1,89 +1,23 @@
 
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Badge from 'react-bootstrap/Badge'
-import {chartResponse} from '../config/yahooChart'
+import { chartResponse } from '../config/yahooChart'
+import { chartDataSet, dateRange } from '../config/price'
 import Table from 'react-bootstrap/Table'
 import { BsFillXCircleFill } from "react-icons/bs";
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 const axios = require('axios').default
 
 export default function Home() {
 
-  const chartDataSet = {
-    fill: false,
-    lineTension: 0.1,
-    borderCapStyle: 'butt',
-    borderDash: [],
-    borderDashOffset: 0.0,
-    borderJoinStyle: 'miter',
-    pointBackgroundColor: '#fff',
-    pointBorderWidth: 1,
-    pointHoverRadius: 5,
-    pointHoverBorderColor: 'rgba(220,220,220,1)',
-    pointHoverBorderWidth: 2,
-    pointRadius: 1,
-    pointHitRadius: 10,
-  }
-
-  const dateRange = [
-    {
-      'fromDate': '2021-01-01',
-      'toDate': '2021-12-31'
-    },
-    {
-      'fromDate': '2020-01-01',
-      'toDate': '2020-12-31'
-    },
-    {
-      'fromDate': '2019-01-01',
-      'toDate': '2019-12-31'
-    },
-    {
-      'fromDate': '2018-01-01',
-      'toDate': '2018-12-31'
-    },
-    {
-      'fromDate': '2017-01-01',
-      'toDate': '2017-12-31'
-    },
-    {
-      'fromDate': '2016-01-01',
-      'toDate': '2016-12-31'
-    },
-    {
-      'fromDate': '2015-01-01',
-      'toDate': '2015-12-31'
-    },
-    {
-      'fromDate': '2014-01-01',
-      'toDate': '2014-12-31'
-    },
-    {
-      'fromDate': '2013-01-01',
-      'toDate': '2013-12-31'
-    },
-    {
-      'fromDate': '2012-01-01',
-      'toDate': '2012-12-31'
-    },
-    {
-      'fromDate': '2011-01-01',
-      'toDate': '2011-12-31'
-    },
-    {
-      'fromDate': '2010-01-01',
-      'toDate': '2010-12-31'
-    },
-  ]
-
   const [tickers, setTickers] = useState([])
   const [tableHeader, setTableHeader] = useState([])
   const [yearlyPcnt, setYearlyPcnt] = useState([])
-  const [chartData, setChartData] = useState({'labels': [...dateRange.map(item=>item.fromDate.substring(0,4) )].reverse(), 'datasets': []})
+  const [chartData, setChartData] = useState({ 'labels': [...dateRange.map(item => item.fromDate.substring(0, 4))].reverse(), 'datasets': [] })
   const [validated, setValidated] = useState(false)
   const [formValue, setFormValue] = useState({})
   const [clicked, setClicked] = useState(false)
@@ -103,7 +37,7 @@ export default function Home() {
       []
     )
     setChartData(
-      {'labels': [...dateRange.map(item=>item.fromDate.substring(0,4) )].reverse(), 'datasets': []}
+      { 'labels': [...dateRange.map(item => item.fromDate.substring(0, 4))].reverse(), 'datasets': [] }
     )
   }
 
@@ -111,27 +45,27 @@ export default function Home() {
     if (clicked) return
 
     setTickers(
-      [...tickers.filter(x=>x!==value)]
+      [...tickers.filter(x => x !== value)]
     )
     setYearlyPcnt(
       [
-        ...yearlyPcnt.filter(x=>x[0]!==value)
+        ...yearlyPcnt.filter(x => x[0] !== value)
       ]
     )
     setChartData(
       {
-        'labels': [...dateRange.map(item=>item.fromDate.substring(0,4) )].reverse(),
-        'datasets': [...chartData.datasets.filter(x=>x.label!==value)]
+        'labels': [...dateRange.map(item => item.fromDate.substring(0, 4))].reverse(),
+        'datasets': [...chartData.datasets.filter(x => x.label !== value)]
       }
     )
   }
 
-  async function  handleTickers (inputTickers) {
+  async function handleTickers(inputTickers) {
 
     let inputItems = []
-    
+
     dateRange.forEach(item => {
-      inputItems.push (
+      inputItems.push(
         inputTickers.map(tickerItem => {
           const newItem = {
             'ticker': tickerItem.toUpperCase(),
@@ -142,9 +76,9 @@ export default function Home() {
         })
       )
     })
-    
 
-    let outputItem = {...chartResponse}
+
+    let outputItem = { ...chartResponse }
     let temp = inputTickers.map(tickerItem => {
       return {
         'ticker': tickerItem.toUpperCase(),
@@ -156,29 +90,30 @@ export default function Home() {
     })
     let query = ''
 
-    for(const tickerItems of inputItems) {
+    for (const tickerItems of inputItems) {
 
-      for(const item of tickerItems) {
+      for (const item of tickerItems) {
         query = `ticker=${item.ticker}&fromdate=${item.fromDate}&todate=${item.toDate}`
         outputItem = await axios(`/api/getYahoo?${query}`)
 
         // outputItem = await getYahooHistoryPrice(item.ticker, item.fromDate, item.toDate)
-        if (outputItem.data.indicators && outputItem.data.indicators.quote[0].close.length > 0) {
-          const opening = outputItem.data.indicators.quote[0].close[0]
-          const closing = outputItem.data.indicators.quote[0].close[outputItem.data.indicators.quote[0].close.length - 1]
-          temp.filter(x=>x.ticker==item.ticker)[0].data.push(((closing - opening) / opening * 100).toFixed(2))
+        const allData = outputItem.data.indicators.quote[0].close
+        if (allData && allData.length > 0) {
+          const opening = allData[0]
+          const closing = allData[allData.length - 1]
+          temp.filter(x => x.ticker == item.ticker)[0].data.push(((closing - opening) / opening * 100).toFixed(2))
 
-          if (!temp.filter(x=>x.ticker==item.ticker)[0].endPrice) temp.filter(x=>x.ticker==item.ticker)[0].endPrice = opening
-          temp.filter(x=>x.ticker==item.ticker)[0].startPrice = closing
-          temp.filter(x=>x.ticker==item.ticker)[0].yearCnt += 1
+          if (!temp.filter(x => x.ticker == item.ticker)[0].endPrice) temp.filter(x => x.ticker == item.ticker)[0].endPrice = opening
+          temp.filter(x => x.ticker == item.ticker)[0].startPrice = closing
+          temp.filter(x => x.ticker == item.ticker)[0].yearCnt += 1
         }
-        else temp.filter(x=>x.ticker==item.ticker)[0].data.push("N/A")
+        else temp.filter(x => x.ticker == item.ticker)[0].data.push("N/A")
       }
     }
 
-    temp = temp.filter(x=> !tickers.includes(x.ticker))
+    temp = temp.filter(x => !tickers.includes(x.ticker))
 
-    temp = temp.map(item=> {
+    temp = temp.map(item => {
       const newTemp = {
         'annualized': getAnnualizedPcnt(item),
         'total': getTotalPcnt(item),
@@ -189,8 +124,8 @@ export default function Home() {
 
     setTickers([
       ...tickers,
-      ...temp.map(item=> item.ticker)
-    ]) 
+      ...temp.map(item => item.ticker)
+    ])
 
     setTableHeader(
       [
@@ -204,7 +139,7 @@ export default function Home() {
     setYearlyPcnt(
       [
         ...yearlyPcnt,
-        ...temp.map(item=> {
+        ...temp.map(item => {
           const newItem = [
             item.ticker,
             item.annualized,
@@ -220,11 +155,11 @@ export default function Home() {
       {
         label: [...dateRange.map(ii => ii.fromDate.substring(0, 4))].reverse(),
         datasets: [...chartData.datasets,
-        ...temp.map(item=> {
+        ...temp.map(item => {
           const r = Math.floor(Math.random() * 255) + 1
           const g = Math.floor(Math.random() * 255) + 1
           const b = Math.floor(Math.random() * 255) + 1
-    
+
           let backgroundColor = (`rgba(${r}, ${g}, ${b}, 0.4)`)
           let borderColor = (`rgba(${r}, ${g}, ${b}, 1)`)
 
@@ -241,9 +176,9 @@ export default function Home() {
         })
         ]
       }
-    )      
+    )
 
-    
+
   }
 
   const getTotalPcnt = (item) => {
@@ -255,20 +190,20 @@ export default function Home() {
     let totalPcnt = 1
 
     //exclude 2021
-    item.data.slice(1).forEach (data => {
+    item.data.slice(1).forEach(data => {
       if (data != 'N/A') {
-        totalPcnt = totalPcnt * (parseFloat(data)/100 + 1)
+        totalPcnt = totalPcnt * (parseFloat(data) / 100 + 1)
       }
     })
 
-    const diffPcnt = ((Math.pow(totalPcnt, 1/(item.yearCnt - 1))  - 1) * 100).toFixed(2)
+    const diffPcnt = ((Math.pow(totalPcnt, 1 / (item.yearCnt - 1)) - 1) * 100).toFixed(2)
 
     return diffPcnt
   }
 
   const getCellColor = (cellValue) => {
-    if (cellValue < 0) return {color: 'red'}
-    else return {color: 'black'}
+    if (cellValue < 0) return { color: 'red' }
+    else return { color: 'black' }
   }
 
   const handleSubmit = async (event) => {
@@ -310,7 +245,7 @@ export default function Home() {
         </Fragment>
         <Row className="pl-3 pt-3">
           {
-            tickers.map((item, index)=>(
+            tickers.map((item, index) => (
               <Badge pill variant="success" key={index} className="ml-1">
                 {item}
                 <BsFillXCircleFill onClick={() => { removeItem(item) }} className="ml-1 mb-1" />
@@ -319,26 +254,26 @@ export default function Home() {
           }
         </Row>
         <Table className="pl-3 mt-3" responsive>
-            <thead>
-              <tr>
-                {tableHeader.map((item, index)=> (
-                  <th key={index}>{item}</th>
-                ))
-                }
+          <thead>
+            <tr>
+              {tableHeader.map((item, index) => (
+                <th key={index}>{item}</th>
+              ))
+              }
 
+            </tr>
+          </thead>
+          <tbody>
+
+            {yearlyPcnt.map((item, index) => (
+              <tr key={index}>
+                {item.map((xx, yy) => <td key={`${index}${yy}`}><span style={getCellColor(item[yy])}>{item[yy]}</span></td>)}
               </tr>
-            </thead>
-            <tbody>
+            ))}
 
-                {yearlyPcnt.map((item, index) => (
-                  <tr key={index}>
-                    {item.map((xx,yy) => <td key={`${index}${yy}`}><span style={getCellColor(item[yy])}>{item[yy]}</span></td>)}                
-                  </tr>                  
-                ))}
-
-            </tbody>
-          </Table>      
-          <Line data={chartData} />  
+          </tbody>
+        </Table>
+        <Line data={chartData} />
       </Container>
     </Fragment>
   )
