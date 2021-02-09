36a21cd4c4906cd65a4446b2cb9cfd9942c4e0ea
So Alex <as2120@ad01.bcthk.com>
2021-02-09 16:19:19 +0800

    sticky sticky

--- components/StockInfoTable.js
+++ components/StockInfoTable.js
@@ -3,40 +3,42 @@ import { Fragment } from 'react'
 import Table from 'react-bootstrap/Table'
 
 const getCellColor = (cellValue) => {
-    if ((cellValue || '').toString().replace(/\%/, '') < 0) return { color: 'red' }
-    else return { color: 'black' }
-  }
+  if ((cellValue || '').toString().replace(/\%/, '') < 0) return { color: 'red' }
+  else return { color: 'black' }
+}
 
 const getCellItem = (item) => {
-  if ((item||'').toString().match(/http:/gi))
+  if ((item || '').toString().match(/http:/gi))
     return <a href={item} target='_blank'>{item}</a>
   else return item
 }
 
+const sticky = { backgroundColor: '#ddd', left: 0, position: 'sticky', zIndex: '997' }
+
 function StockInfoTable({ tableHeader, tableData, sortItem }) {
-  
+
   return (
-    <Fragment>     
-        <Table className="pl-3 mt-3" responsive>
-          <thead>
-            <tr>
-              {tableHeader.map((item, index) => (
-                <th onClick={() => { sortItem(index) }} key={index}>{item}</th>
-              ))
-              }
+    <Fragment>
+      <Table className="pl-3 mt-3" responsive>
+        <thead>
+          <tr>
+            {tableHeader.map((item, index) => (
+              <th style={(index == 0 ? sticky : {})} onClick={() => { sortItem(index) }} key={index} >{item}</th>
+            ))
+            }
 
+          </tr>
+        </thead>
+        <tbody>
+
+          {tableData.map((item, index) => (
+            <tr key={index}>
+              {item.map((xx, yy) => <td style={(yy == 0 ? sticky : {})} key={`${index}${yy}`}><span style={getCellColor(xx)}>{getCellItem(xx)}</span></td>)}
             </tr>
-          </thead>
-          <tbody>
+          ))}
 
-            {tableData.map((item, index) => (
-              <tr key={index}>
-                {item.map((xx, yy) => <td key={`${index}${yy}`}><span style={getCellColor(xx)}>{getCellItem(xx)}</span></td>)}
-              </tr>
-            ))}
-
-          </tbody>
-        </Table>
+        </tbody>
+      </Table>
     </Fragment>
   )
 }
--- config/etf.js
+++ config/etf.js
@@ -1,3 +1,4 @@
-const selectedHeaders = "Issuer,Structure,Expense Ratio,Inception,Index Tracked,Category,Asset Class,52 Week Lo,52 Week Hi,AUM,1 Month Avg. Volume,3 Month Avg. Volume"
+//const selectedHeaders = "Issuer,Structure,Expense Ratio,Inception,Index Tracked,Category,Asset Class,52 Week Lo,52 Week Hi,AUM,1 Month Avg. Volume,3 Month Avg. Volume"
+const selectedHeaders = "Issuer,Expense Ratio,Inception,Index Tracked,Category,52 Week Lo,52 Week Hi,AUM,1 Month Avg. Volume,3 Month Avg. Volume"
 
 export const selectedHeadersArr = selectedHeaders.split(',')
