f574bc1498023942255b63e385fac6be939b5433
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-02-11 18:06:09 +0800

    use back tickers

--- pages/etfdetail.js
+++ pages/etfdetail.js
@@ -73,7 +73,7 @@ export default function Home() {
     })
 
     const pieData = {
-      labels: [...holdingInfo.map(item => item[1])],
+      labels: [...holdingInfo.map(item => item.find(x => x))],
       datasets: [
         {
           label: '# of Holdings',
