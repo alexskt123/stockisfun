e7ef0fafe6452165c34ddb7129e3c1b6f29a59d0
So Alex <as2120@ad01.bcthk.com>
2021-02-23 13:04:57 +0800

    new structure for sending email

---
+++ pages/api/sendEmailByCategory.js
@@ -0,0 +1,26 @@
+
+import { getEmailsByCategory } from '../../lib/firebaseResult'
+
+const axios = require('axios').default
+
+export default async (req, res) => {
+
+    const { category } = req.query
+
+    const emails = await getEmailsByCategory(category)
+
+    let response = {
+        response: 'NOT OK'
+    }
+
+    await axios.all(emails.map(email => {
+        return axios.get(`https://stockisfun.vercel.app/api/sendEmailForMA?id=${email.id}`).catch(err => console.log(err))
+    }))
+        .catch(error => console.log(error))
+        .then((_responses) => {
+            response.response = 'OK'
+        })
+
+    res.statusCode = 200
+    res.json(response)
+}
