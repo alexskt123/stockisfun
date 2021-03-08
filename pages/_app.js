0b29822ed11f741c03bb6414236b5701a960e3ae
So Alex <as2120@ad01.bcthk.com>
2021-03-08 11:06:11 +0800

    try with meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,6 +10,33 @@ import { StateProvider } from '../lib/store'
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Stock Is Fun</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/icon.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />        
+        <link
+          href="/icons/icon.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <StateProvider>
         <Layout>
           <Component {...pageProps} />
