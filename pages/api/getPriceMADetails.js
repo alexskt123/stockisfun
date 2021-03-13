ae7f1e082b7cc32db10d268ca5470fc75677cef0
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-03-13 20:24:25 +0800

    refactor email, add industry

--- pages/api/getPriceMADetails.js
+++ pages/api/getPriceMADetails.js
@@ -1,12 +1,14 @@
 
 import { getYahooHistoryPrice } from '../../lib/yahoo/getYahooHistoryPrice'
 import { getYahooQuote } from '../../lib/yahoo/getYahooQuote'
+import { getYahooAssetProfile } from '../../lib/yahoo/getYahooAssetProfile'
 import moment from 'moment-business-days'
 import { ma } from 'moving-averages'
 import QuickChart from 'quickchart-js'
 import { getFormattedFromToDate } from '../../lib/commonFunction'
-import { priceChartSettings, ma5ChartSettings, ma20ChartSettings, ma60ChartSettings} from '../../config/price'
-
+import { priceChartSettings, ma5ChartSettings, ma20ChartSettings, ma60ChartSettings } from '../../config/price'
+import { priceMAList } from '../../config/email'
+import millify from 'millify'
 
 const handleDays = async (ticker, fromdate, todate) => {
 
@@ -20,11 +22,13 @@ const handleDays = async (ticker, fromdate, todate) => {
 
 const handleQuote = async (ticker) => {
   const quote = await getYahooQuote(ticker)
+  const assetProfile = await getYahooAssetProfile(ticker)
 
   return {
     Name: quote.longName,
     Price: quote.regularMarketPrice,
-    MarketCap: quote.marketCap
+    MarketCap: millify(quote.marketCap),
+    Industry: assetProfile.industry
   }
 }
 
@@ -71,16 +75,16 @@ const getImgUrl = async (ticker, dateprice, ma5, ma20, ma60) => {
   return newChart.getUrl()
 }
 
-const chkLower = async (trackArr, refArr) => {
+const chkLower = (trackArr, refArr) => {
   if (trackArr[trackArr.length - 1] >= refArr[refArr.length - 1]
-        && trackArr.find(x => x) < refArr.find(x => x)) {
+    && trackArr.find(x => x) < refArr.find(x => x)) {
     return true
   } else return false
 }
 
-const chkHigher = async (trackArr, refArr) => {
+const chkHigher = (trackArr, refArr) => {
   if (trackArr[trackArr.length - 1] <= refArr[refArr.length - 1]
-        && trackArr.find(x => x) > refArr.find(x => x)) {
+    && trackArr.find(x => x) > refArr.find(x => x)) {
     return true
   } else return false
 }
@@ -92,83 +96,47 @@ export default async (req, res) => {
 
   const fromtodate = await getFormattedFromToDate(80)
   const dateprice = await handleDays(ticker, fromtodate.formattedFromDate, fromtodate.formattedToDate)
-  const quote = await handleQuote(ticker)
-  const tickerInfo = {
-    'Symbol': ticker,
-    ...quote
-  }
-
-  const fiveLowerTwenty = []
-  const fiveLowerTwentyChart = []
-  const fiveHigherTwenty = []
-  const fiveHigherTwentyChart = []
-
-  const fiveLowerSixty = []
-  const fiveLowerSixtyChart = []
-  const fiveHigherSixty = []
-  const fiveHigherSixtyChart = []
-
-  const twentyLowerSixty = []
-  const twentyLowerSixtyChart = []
-  const twentyHigherSixty = []
-  const twentyHigherSixtyChart = []
 
   const asOfDate = dateprice.date.reverse().find(x => x)
-
   const ma5 = ma([...dateprice.price], 5)
   const ma20 = ma([...dateprice.price], 20)
   const ma60 = ma([...dateprice.price], 60)
-
   const ma5filter = [...ma5].reverse().slice(0, 2)
   const ma20filter = [...ma20].reverse().slice(0, 2)
   const ma60filter = [...ma60].reverse().slice(0, 2)
 
-  if (ma5filter.length == 2 && ma20filter.length == 2 && ma60filter.length == 2) {
-    if (await chkLower(ma5filter, ma20filter)) {
-      fiveLowerTwenty.push(tickerInfo)
-      if (isGenChart) fiveLowerTwentyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
+  const newPriceMAList = !(ma5filter.length == 2 && ma20filter.length == 2 && ma60filter.length == 2)
+    ? [...priceMAList]
+    : await Promise.all([...priceMAList].map(async item => {
+      const matches = item.id === '5<20' ? chkLower(ma5filter, ma20filter)
+        : item.id === '5>20' ? chkHigher(ma5filter, ma20filter)
+          : item.id === '5<60' ? chkLower(ma5filter, ma60filter)
+            : item.id === '5>60' ? chkHigher(ma5filter, ma60filter)
+              : item.id === '20<60' ? chkLower(ma20filter, ma60filter)
+                : item.id === '20>60' ? chkHigher(ma20filter, ma60filter)
+                  : false
 
-    if (await chkHigher(ma5filter, ma20filter)) {
-      fiveHigherTwenty.push(tickerInfo)
-      if (isGenChart) fiveHigherTwentyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
+      const newItem = { ...item, tickerInfo: [], tickersChart: [] }
+      if (matches) {
+        const quote = await handleQuote(ticker)
+        const tickerInfo = {
+          'Symbol': ticker.toUpperCase(),
+          ...quote
+        }
 
-    if (await chkLower(ma5filter, ma60filter)) {
-      fiveLowerSixty.push(tickerInfo)
-      if (isGenChart) fiveLowerSixtyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
+        newItem.tickersInfo = [tickerInfo]
 
-    if (await chkHigher(ma5filter, ma60filter)) {
-      fiveHigherSixty.push(tickerInfo)
-      if (isGenChart) fiveHigherSixtyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
-
-    if (await chkLower(ma20filter, ma60filter)) {
-      twentyLowerSixty.push(tickerInfo)
-      if (isGenChart) twentyLowerSixtyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
-
-    if (await chkHigher(ma20filter, ma60filter)) {
-      twentyHigherSixty.push(tickerInfo)
-      if (isGenChart) twentyHigherSixtyChart.push(await getImgUrl(ticker, dateprice, ma5, ma20, ma60))
-    }
-  }
+        if (isGenChart) {
+          const imgUrl = await getImgUrl(ticker, dateprice, ma5, ma20, ma60)
+          newItem.tickersChart = [imgUrl]
+        }
+      }
+      return newItem
+    }))
 
   res.statusCode = 200
   res.json({
     asOfDate,
-    fiveLowerTwenty,
-    fiveLowerTwentyChart,
-    fiveHigherTwenty,
-    fiveHigherTwentyChart,
-    fiveLowerSixty,
-    fiveLowerSixtyChart,
-    fiveHigherSixty,
-    fiveHigherSixtyChart,
-    twentyLowerSixty,
-    twentyLowerSixtyChart,
-    twentyHigherSixty,
-    twentyHigherSixtyChart
+    priceMAList: [...newPriceMAList]
   })
 }
--- pages/api/sendEmailForMA.js
+++ pages/api/sendEmailForMA.js
@@ -2,13 +2,15 @@
 import sendEmail from '../../lib/sendEmail'
 import { getEmailByID } from '../../lib/firebaseResult'
 import { getHost } from '../../lib/commonFunction'
-import millify from 'millify'
 
 const axios = require('axios').default
 
+const getUrlItem = (item) => {
+  return `<u><a href="${getHost()}/stockdetail?query=${item}">${item}</a></u>`
+}
+
 export default async (req, res) => {
 
-    
   const response = {
     response: 'NOT OK'
   }
@@ -21,20 +23,48 @@ export default async (req, res) => {
   const tickerArr = curEmailTemplate.stock.split(',').map(item => item.toUpperCase())
   const genChart = curEmailTemplate.genChart ? true : false
 
+  // no idea why cannot use spread priceMAlist here
   const priceMADetails = {
     asOfDate: '',
-    fiveLowerTwenty: [],
-    fiveLowerTwentyChart: [],
-    fiveHigherTwenty: [],
-    fiveHigherTwentyChart: [],
-    fiveLowerSixty: [],
-    fiveLowerSixtyChart: [],
-    fiveHigherSixty: [],
-    fiveHigherSixtyChart: [],
-    twentyLowerSixty: [],
-    twentyLowerSixtyChart: [],
-    twentyHigherSixty: [],
-    twentyHigherSixtyChart: [],
+    priceMAList: [
+      {
+        id: '5<20',
+        name: '5-MA lower than 20-MA',
+        tickersInfo: [],
+        tickersChart: []
+      },
+      {
+        id: '5>20',
+        name: '5-MA higher than 20-MA',
+        tickersInfo: [],
+        tickersChart: []
+      },
+      {
+        id: '5<60',
+        name: '5-MA lower than 60-MA',
+        tickersInfo: [],
+        tickersChart: []
+      },
+      {
+        id: '5>60',
+        name: '5-MA higher than 60-MA',
+        tickersInfo: [],
+        tickersChart: []
+      },
+      {
+        id: '20<60',
+        name: '20-MA lower than 60-MA',
+        tickersInfo: [],
+        tickersChart: []
+      },
+      {
+        id: '20>60',
+        name: '20-MA higher than 60-MA',
+        tickersInfo: [],
+        tickersChart: []
+      }
+
+    ]
   }
 
   await axios.all(tickerArr.map(ticker => {
@@ -48,121 +78,60 @@ export default async (req, res) => {
         responses.forEach(item => {
           if (item && item.data) {
             priceMADetails.asOfDate = priceMADetails.asOfDate == '' ? item.data.asOfDate : priceMADetails.asOfDate
-            priceMADetails.fiveLowerTwenty.push(...item.data.fiveLowerTwenty)
-            priceMADetails.fiveLowerTwentyChart.push(...item.data.fiveLowerTwentyChart)
-            priceMADetails.fiveHigherTwenty.push(...item.data.fiveHigherTwenty)
-            priceMADetails.fiveHigherTwentyChart.push(...item.data.fiveHigherTwentyChart)
-            priceMADetails.fiveLowerSixty.push(...item.data.fiveLowerSixty)
-            priceMADetails.fiveLowerSixtyChart.push(...item.data.fiveLowerSixtyChart)
-            priceMADetails.fiveHigherSixty.push(...item.data.fiveHigherSixty)
-            priceMADetails.fiveHigherSixtyChart.push(...item.data.fiveHigherSixtyChart)
-            priceMADetails.twentyLowerSixty.push(...item.data.twentyLowerSixty)
-            priceMADetails.twentyLowerSixtyChart.push(...item.data.twentyLowerSixtyChart)
-            priceMADetails.twentyHigherSixty.push(...item.data.twentyHigherSixty)
-            priceMADetails.twentyHigherSixtyChart.push(...item.data.twentyHigherSixtyChart)
+            priceMADetails.priceMAList.forEach(cur => {
+              const priceMA = item.data.priceMAList.find(x => x.id === cur.id)
+              cur.tickersInfo.push(...priceMA.tickersInfo)
+              cur.tickersChart.push(...priceMA.tickersChart)
+            })
           }
-        })                
+        })
       }
     })
-
-  const fiveLowerTwentyList = priceMADetails.fiveLowerTwenty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.fiveLowerTwentyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`
-    return acc
-  }, '')
-  const fiveHigherTwentyList = priceMADetails.fiveHigherTwenty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.fiveHigherTwentyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`   
-    return acc
-  }, '')
-  const fiveLowerSixtyList = priceMADetails.fiveLowerSixty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.fiveLowerSixtyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`
-    return acc
-  }, '')
-  const fiveHigherSixtyList = priceMADetails.fiveHigherSixty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.fiveHigherSixtyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`  
-    return acc
-  }, '')
-  const twentyLowerSixtyList = priceMADetails.twentyLowerSixty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.twentyLowerSixtyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`
-    return acc
-  }, '')
-  const twentyHigherSixtyList = priceMADetails.twentyHigherSixty.reduce((acc, cur, index) => {
-    const imgElement = genChart ? `<img src=${priceMADetails.twentyHigherSixtyChart[index]}/>` : ''
-    acc += `<li><p>Symbol: <a href="${getHost()}/stockdetail?query=${cur.Symbol}">${cur.Symbol}</a></p><p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p><p>Market Cap.: ${millify(cur.MarketCap)}</p>${imgElement}</li>`
-    return acc
-  }, '')
-  const inputArrList = tickerArr.reduce((acc, cur) => {
-    acc += `<li>${cur}</li>`
-    return acc
-  }, '')
 
   const mailOptions = {
     from: process.env.EMAIL,
     bcc: curEmailTemplate.to,
     subject: `Moving Average Highlight - As of ${priceMADetails.asOfDate}`,
     html: `
-        <p>
-            <h5>${curEmailTemplate.name}</h5>
-        </p>        
-        <p>
-            <b>As of ${priceMADetails.asOfDate}:</b>
-        </p>
-        <hr>
-        <p>
-            <b>5-MA lower than 20-MA</b>
-            <ol>
-                ${fiveLowerTwentyList}
-            </ol>
-        </p>
-        <hr>
-        <p>
-            <b>5-MA higher than 20-MA</b>
-            <ol>
-                ${fiveHigherTwentyList}
-            </ol>
-        </p>
-        <hr>
-        <p>
-            <b>5-MA lower than 60-MA</b>
-            <ol>
-                ${fiveLowerSixtyList}
-            </ol>
-        </p>
-        <hr>
-        <p>
-            <b>5-MA higher than 60-MA</b>
-            <ol>
-                ${fiveHigherSixtyList}
-            </ol>
-        </p>
-        <hr> 
-        <p>
-            <b>20-MA lower than 60-MA</b>
-            <ol>
-                ${twentyLowerSixtyList}
-            </ol>
-        </p>
-        <hr>
-        <p>
-            <b>20-MA higher than 60-MA</b>
-            <ol>
-                ${twentyHigherSixtyList}
-            </ol>
-        </p>
-        <hr>       
-        <p>
-            <b>Grabbing Ticker List:</b>
-            <ol>
-                ${inputArrList}
-            </ol>
-        </p>
-        `
+          <p>
+              <h5>${curEmailTemplate.name}</h5>
+          </p>        
+          <p>
+              <b>As of ${priceMADetails.asOfDate}:</b>
+          </p>
+          ${priceMADetails.priceMAList.map(priceMA => {
+      return (
+        `<hr>
+                <p>
+                    <b>${priceMA.name}</b>
+                    <ol>
+                      ${priceMA.tickersInfo.map((cur, idx) => {
+          const imgElement = genChart ? `<img src=${priceMA.tickersChart[idx]}/>` : ''
+          return (
+            `<li>
+                            <p>Symbol: ${getUrlItem(cur.Symbol)}</p>
+                            <p>Name: ${cur.Name}</p><p>Market Price: ${cur.Price}</p>
+                            <p>Market Cap.: ${cur.MarketCap}</p>
+                            <p>Industry: ${cur.Industry}</p>
+                            <p>${imgElement}</p>
+                          </li>`
+          )
+        }).join('')}
+                    </ol>
+                </p>
+                `
+      )
+    }).join('')
+      }
+          <hr>       
+          <p>
+              <b>Grabbing Ticker List:</b>
+              <ol>
+                  ${tickerArr.map(item=>`<li>${getUrlItem(item)}</li>`).join('')}
+              </ol>
+          </p>
+          `
   }
-
 
   await sendEmail(mailOptions)
 
