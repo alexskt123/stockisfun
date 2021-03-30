c26abe934e73ae1b1dfd85d110b2ac93daf5ef13
So Alex <as2120@ad01.bcthk.com>
2021-03-30 12:45:21 +0800

    show share button only have query

--- pages/watchlist.js
+++ pages/watchlist.js
@@ -150,7 +150,7 @@ export default function WatchList() {
                 ? <Button className="ml-2" onClick={() => { setShowUpdate(true) }} size='sm' variant='dark' >{'Update Watch List'}</Button>
                 : null
             }
-            <HappyShare inputStyle={{ color: 'blue', size: '25px' }}/>
+            { query ? <HappyShare inputStyle={{ color: 'blue', size: '25px' , style:{backgroundColor: 'black'}}}/> : null}
           </Row>
 
           {
