c81c492b75ccb93720a4f0ceab2a55c27dccba45
So Alex <as2120@ad01.bcthk.com>
2021-04-13 08:49:13 +0800

    pass only tickers and years instead of whole settings to PriceChange - preparation for SWR

--- pages/compare/price.js
+++ pages/compare/price.js
@@ -2,31 +2,30 @@
 import { Fragment, useState } from 'react'
 import { useRouter } from 'next/router'
 
-import { dateRange, dateRangeByNoOfYears } from '../../config/price'
 import TickerInput from '../../components/Page/TickerInput'
 import TickerBullet from '../../components/Page/TickerBullet'
 import CustomContainer from '../../components/Layout/CustomContainer'
-import LoadingSpinner from '../../components/Loading/LoadingSpinner'
 import PriceChange from '../../components/Parts/PriceChange'
 
-import { getPriceInfo, priceSettingSchema, handleDebounceChange, handleFormSubmit } from '../../lib/commonFunction'
+import { handleDebounceChange, handleFormSubmit } from '../../lib/commonFunction'
 import { useQuery } from '../../lib/hooks/useQuery'
 
 export default function ComparePrice() {
   const router = useRouter()
   const { query, year } = router.query
 
-  const [settings, setSettings] = useState(priceSettingSchema)
-  const [newDateRange, setNewDateRange] = useState(dateRange)
+  const [settings, setSettings] = useState({tickers: [], years: 15})
   const [validated, setValidated] = useState(false)
-  const [formValue, setFormValue] = useState({})
-  const [clicked, setClicked] = useState(false)
+  const [formValue, setFormValue] = useState({})  
 
   const handleChange = async (e) => {
     if (e.target.name == 'formYear') {
-      const newDateArr = await dateRangeByNoOfYears(e.target.value)      
-      setNewDateRange(newDateArr)
-      setSettings({...priceSettingSchema, chartData: { 'labels': [...newDateArr.map(item => item.fromDate.substring(0, 4))].reverse(), 'datasets': [] }})
+      setSettings({
+        ...settings,
+        years: e.target.value
+      })
+
+      handleFormSubmit(e, formValue, { query, year: e.target.value}, router, setValidated)
     }
     handleDebounceChange(e, formValue, setFormValue)
   }
@@ -35,9 +34,7 @@ export default function ComparePrice() {
     setSettings({
       ...settings,
       tickers: [],
-      yearlyPcnt: [],
-      quote: [],
-      chartData: { 'labels': [...newDateRange.map(item => item.fromDate.substring(0, 4))].reverse(), 'datasets': [] }
+      years: 15
     })
     router.push(router.pathname)
   }
@@ -46,24 +43,18 @@ export default function ComparePrice() {
     setSettings(
       {
         ...settings,
-        tickers: [...settings.tickers.filter(x => x !== value)],
-        yearlyPcnt: [...settings.yearlyPcnt.filter(x => x.find(x => x) !== value)],
-        chartData: {
-          'labels': [...newDateRange.map(item => item.fromDate.substring(0, 4))].reverse(),
-          'datasets': [...settings.chartData.datasets.filter(x => x.label !== value)]
-        }
+        tickers: [...settings.tickers.filter(x => x !== value)]
       }
     )
   }
 
   async function handleTickers(inputTickers, inputYear) {
 
-    setClicked(true)
-
-    const priceInfo = await getPriceInfo(inputTickers, inputYear ? inputYear : 15, settings)
-    setSettings(priceInfo)
-
-    setClicked(false)
+    setSettings({
+      ...settings,
+      tickers: inputTickers,
+      years: inputYear
+    })
   }
 
   const handleSubmit = async (event) => {
@@ -81,18 +72,12 @@ export default function ComparePrice() {
             handleSubmit={handleSubmit}
             placeholderText={'Single:  aapl /  Mulitple:  tsm,0700.hk,voo'}
             handleChange={handleChange}
-            clicked={clicked}
             clearItems={clearItems}
-            tableHeader={settings.tableHeader}
-            tableData={settings.yearlyPcnt}
             exportFileName={'Stock_price.csv'}
             yearControl={true}
           />
           <TickerBullet tickers={settings.tickers} overlayItem={settings.quote} removeItem={removeItem} />
-          {clicked ?
-            <LoadingSpinner /> : null
-          }
-          <PriceChange inputSettings={settings} />
+          <PriceChange inputTickers={settings.tickers} inputYear={settings.years} />
         </Fragment>
       </CustomContainer>
     </Fragment >
