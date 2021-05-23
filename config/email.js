c4d3610c4f9a05354c41cd1a227cb70e8e007b06
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-05-23 21:43:32 +0800

    move sequence of  summary table

--- config/email.js
+++ config/email.js
@@ -8,20 +8,8 @@ export const priceMAList = [
     tickersChart: []
   },
   {
-    id: '5>20',
-    name: '5-MA higher than 20-MA',
-    tickersInfo: [],
-    tickersChart: []
-  },
-  {
     id: '5<60',
     name: '5-MA lower than 60-MA',
-    tickersInfo: [],
-    tickersChart: []
-  },
-  {
-    id: '5>60',
-    name: '5-MA higher than 60-MA',
     tickersInfo: [],
     tickersChart: []
   },
@@ -32,10 +20,21 @@ export const priceMAList = [
     tickersChart: []
   },
   {
+    id: '5>20',
+    name: '5-MA higher than 20-MA',
+    tickersInfo: [],
+    tickersChart: []
+  },
+  {
+    id: '5>60',
+    name: '5-MA higher than 60-MA',
+    tickersInfo: [],
+    tickersChart: []
+  },
+  {
     id: '20>60',
     name: '20-MA higher than 60-MA',
     tickersInfo: [],
     tickersChart: []
   }
-
 ]
