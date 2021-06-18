30a9e878b8467203d82e5654b7b3a0a5104314d7
Li Eddy <el2083@ad01.bcthk.com>
2021-06-18 14:14:35 +0800

    1. unsubscribe firebase real time data stream in the useEffect hook
    2. handle user and uid inside userdata

--- components/Parts/WatchListSuggestions.js
+++ components/Parts/WatchListSuggestions.js
@@ -7,41 +7,41 @@ import { getHighlistWatchList, useUser, useUserData } from '../../lib/firebaseRe
 import { randVariant } from '../../lib/commonFunction'
 
 function WatchListSuggestions({ onClickWatchListButton }) {
-    const user = useUser()
-    const userData = useUserData(user?.uid || '')
+  const user = useUser()
+  const userData = useUserData(user)
 
-    const [watchList, setWatchList] = useState([])
+  const [watchList, setWatchList] = useState([])
 
-    useEffect(async () => {
-        const watchList = await getHighlistWatchList()
-        const { watchList: userWatchList, boughtList } = userData
-        const appendWatchList = userWatchList && boughtList ? {'Watch List': userWatchList, 'Bought List': boughtList.map(item => item.ticker)} : {}
+  useEffect(async () => {
+    const watchList = await getHighlistWatchList()
+    const { watchList: userWatchList, boughtList } = userData
+    const appendWatchList = userWatchList && boughtList ? { 'Watch List': userWatchList, 'Bought List': boughtList.map(item => item.ticker) } : {}
 
-        const newWatchList = {
-            ...watchList,
-            ...appendWatchList
+    const newWatchList = {
+      ...watchList,
+      ...appendWatchList
+    }
+    setWatchList(newWatchList)
+  }, [userData])
+
+  return (
+    <Fragment>
+      <Row className="justify-content-center mt-1">
+        <h6>
+          <Badge style={{ minWidth: '9rem' }} variant="dark">{'Live Watch'}</Badge>
+        </h6>
+      </Row>
+      <Row className='justify-content-center mt-1'>
+        {
+          Object.keys(watchList).map((key, idx) => {
+            return <Button size="sm" key={idx} className="ml-2" variant={randVariant(key)} onClick={() => onClickWatchListButton(key, watchList[key])}>
+              <Badge>{key}</Badge>
+            </Button>
+          })
         }
-        setWatchList(newWatchList)
-    }, [userData])
-
-    return (
-        <Fragment>
-            <Row className="justify-content-center mt-1">
-                <h6>
-                    <Badge style={{ minWidth: '9rem' }} variant="dark">{'Live Watch'}</Badge>
-                </h6>
-            </Row>
-            <Row className='justify-content-center mt-1'>
-                {
-                    Object.keys(watchList).map((key, idx) => {
-                        return <Button size="sm" key={idx} className="ml-2" variant={randVariant(key)} onClick={() => onClickWatchListButton(key, watchList[key])}>
-                            <Badge>{key}</Badge>
-                        </Button>
-                    })
-                }
-            </Row>
-        </Fragment>
-    )
+      </Row>
+    </Fragment>
+  )
 }
 
 export default WatchListSuggestions
