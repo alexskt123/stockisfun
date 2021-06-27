c01b7c1a94ce818dfa675e0b18ed073c2008c7e9
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-06-27 18:04:22 +0800

    use skeleton loading for SWR table

---
+++ components/Loading/LoadingSkeleton.js
@@ -0,0 +1,22 @@
+import { Fragment } from 'react'
+
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+
+import { loadingSkeletonColors } from '../../config/settings'
+import { useLoadingSkeletonColor } from '../../lib/hooks/useLoadingSkeletonColor'
+
+const LoadingSkeleton = () => {
+  const colors = useLoadingSkeletonColor(loadingSkeletonColors)
+  return (
+    <Fragment>
+      <SkeletonTheme
+        color={colors.color}
+        highlightColor={colors.highlightColor}
+      >
+        <Skeleton />
+      </SkeletonTheme>
+    </Fragment>
+  )
+}
+
+export default LoadingSkeleton
