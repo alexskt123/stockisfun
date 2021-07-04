d7923463a204f176689e76325ab57e26d9ade1fd
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-07-04 16:13:47 +0800

    Admin Email subscription

---
+++ components/Parts/EmailSubscriptionCard.js
@@ -0,0 +1,120 @@
+import { Fragment, useEffect, useState } from 'react'
+
+import { useUser, useUserData } from '@/lib/firebaseResult'
+import { updUserEmailConfig } from '@/lib/firebaseResult'
+import { fireToast } from '@/lib/toast'
+import validator from 'email-validator'
+import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+
+export default function EmailSubscriptionCard({ item, minWidth }) {
+  const user = useUser()
+  const userData = useUserData(user)
+
+  const [inputData, setInputData] = useState(item)
+
+  useEffect(() => {
+    const userEmailData = userData?.emailConfig?.find(x => x.id === item.id)
+    setInputData(userEmailData || item)
+  }, [item, userData])
+
+  const handleDataChange = (e, itemKey) => {
+    const newData = {
+      ...inputData,
+      [itemKey]: e.target.value
+    }
+    setInputData(newData)
+  }
+
+  const onSubscribe = async () => {
+    const validEmail = validator.validate(inputData.to)
+
+    if (!validEmail) {
+      fireToast({
+        icon: 'error',
+        title: 'Invalid Email'
+      })
+      return
+    }
+
+    const newInputData = { ...inputData, subscribe: !inputData.subscribe }
+    setInputData(newInputData)
+
+    const newEmailList = [
+      ...(userData?.emailConfig || []).filter(x => x.id !== newInputData.id),
+      newInputData
+    ]
+
+    await updUserEmailConfig(user.uid, newEmailList)
+
+    fireToast({
+      icon: 'success',
+      title: 'Updated'
+    })
+  }
+
+  const inputSettings = [
+    {
+      label: 'To Email',
+      value: inputData?.to,
+      key: 'to'
+    },
+    {
+      label: 'Subject',
+      value: inputData?.subject,
+      key: 'subject'
+    }
+  ]
+
+  return (
+    <Fragment>
+      <Card
+        text={'dark'}
+        border={'light'}
+        style={{
+          ['minWidth']: minWidth ? minWidth : '10rem',
+          backgroundColor: '#f5f7f2'
+        }}
+      >
+        {inputData?.name ? (
+          <Card.Header style={{ padding: '0.2rem' }}>
+            <div style={{ display: 'flex', alignItems: 'baseline' }}>
+              <h5>
+                <Badge variant="secondary">
+                  <b>{inputData.name}</b>
+                </Badge>
+              </h5>
+            </div>
+          </Card.Header>
+        ) : null}
+        <Card.Body style={{ padding: '0.2rem' }}>
+          <Form.Group>
+            {inputSettings.map((curInput, idx) => {
+              return (
+                <Fragment key={idx}>
+                  <Form.Label>
+                    <Badge variant="dark">{curInput.label}</Badge>
+                  </Form.Label>
+                  <Form.Control
+                    className="w-100"
+                    size="sm"
+                    value={curInput.value}
+                    onChange={e => handleDataChange(e, curInput.key)}
+                  />
+                </Fragment>
+              )
+            })}
+          </Form.Group>
+          <Badge
+            className="cursor"
+            variant={inputData?.subscribe ? 'danger' : 'success'}
+            onClick={() => onSubscribe()}
+          >
+            {inputData?.subscribe ? 'UnSubscribe' : 'Subscribe'}
+          </Badge>
+        </Card.Body>
+      </Card>
+    </Fragment>
+  )
+}
