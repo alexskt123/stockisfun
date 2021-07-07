758f235f5683b3ed8faf66638d8e71b702f45c19
So Alex <as2120@ad01.bcthk.com>
2021-07-07 16:04:57 +0800

    fine tune layout, show trend and calendar

--- components/Layout/Footer/AppContent.js
+++ components/Layout/Footer/AppContent.js
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router'
 import Badge from 'react-bootstrap/Badge'
 import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav'
-import { GiBirdTwitter } from 'react-icons/gi'
+import { BiTrendingUp } from 'react-icons/bi'
+import { GoCalendar } from 'react-icons/go'
 import { RiProfileLine, RiPagesLine } from 'react-icons/ri'
 
 const defaultNavElements = [
@@ -18,9 +19,14 @@ const defaultNavElements = [
     icon: <RiPagesLine />
   },
   {
-    label: 'Bird',
-    href: '/compare/birdmouth',
-    icon: <GiBirdTwitter />
+    label: 'Trend',
+    href: '/trend',
+    icon: <BiTrendingUp />
+  },
+  {
+    label: 'Calendar',
+    href: '/calendar',
+    icon: <GoCalendar />
   }
 ]
 
@@ -33,18 +39,17 @@ const AppContent = () => {
         const active = router.pathname === item.href
         return (
           <Col key={idx}>
-            <Badge
-              style={{ display: 'flex', alignItems: 'center' }}
-              className="ml-1 justify-content-center"
-              variant="dark"
-            >
-              {item.icon}
-              <Link href={item.href} passHref>
-                <Nav.Link active={active} disabled={active}>
+            <Link href={item.href} passHref>
+              <Nav.Link active={active} disabled={active}>
+                <Badge
+                  style={{ display: 'flex', alignItems: 'center' }}
+                  variant="dark"
+                >
+                  {item.icon}
                   {`${item.label}`}
-                </Nav.Link>
-              </Link>
-            </Badge>
+                </Badge>
+              </Nav.Link>
+            </Link>
           </Col>
         )
       })}
