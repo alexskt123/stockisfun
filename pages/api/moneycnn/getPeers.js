dee5fd8b76f6117acb34cdf5bf375f07ae145cbd
So Alex <as2120@ad01.bcthk.com>
2021-07-08 17:25:08 +0800

    fix getPeers

--- pages/api/moneycnn/getPeers.js
+++ pages/api/moneycnn/getPeers.js
@@ -3,10 +3,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { extractYahooInfo } from '@/config/peers'
-import { getHost, millify } from '@/lib/commonFunction'
+import { millify } from '@/lib/commonFunction'
 import { getPeers } from '@/lib/moneycnn/getPeers'
-
-const axios = require('axios').default
+import { getQuote } from '@/lib/yahoo/getQuote'
 
 const getFormattedValue = (format, value) => {
   return format === 'millify' ? millify(value) : value
@@ -17,14 +16,12 @@ export default async (req, res) => {
 
   const data = await getPeers(ticker)
 
-  const responses = await Promise.all(
-    data.map(async item =>
-      axios.get(`${getHost()}/api/yahoo/getQuote?ticker=${item.Ticker}`)
-    )
+  const quotes = await Promise.all(
+    data.map(async item => await getQuote(item.Ticker))
   )
 
   const newData = [...data].map(item => {
-    const data = responses.find(x => x?.data?.symbol === item.Ticker)?.data
+    const data = quotes.find(x => x?.symbol === item.Ticker)
 
     const newItem = {
       ...item,
