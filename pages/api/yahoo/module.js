974bda07bb3449dd528b320d81b91ec3dccfdeb2
KAM TSUN SO <alexskt123@KAMde-MacBook-Air.local>
2021-07-13 20:12:44 +0800

    add other modules to module api

--- pages/api/yahoo/module.js
+++ pages/api/yahoo/module.js
@@ -1,3 +1,4 @@
+import { yahooTrendHeader } from '@/config/forecast'
 import { ModuleQuote } from '@/lib/quote'
 
 const moduleMapping = {
@@ -12,6 +13,73 @@ const moduleMapping = {
     handleData: data => {
       return data?.quoteSummary?.result.find(x => x)?.balanceSheetHistory
         ?.balanceSheetStatements
+    }
+  },
+  RecommendTrend: {
+    name: 'recommendationTrend',
+    handleData: data => {
+      const trend = data?.quoteSummary.result
+        .find(x => x)
+        .recommendationTrend.trend.find(x => x)
+      const returnArr = Object.values(trend || {}).slice(1)
+
+      const dataArr = [...Array(5)].map((_item, idx) => {
+        return returnArr[idx] ? returnArr[idx] : 'N/A'
+      })
+
+      const newData = {
+        ...yahooTrendHeader.reduce((acc, item, idx) => {
+          return { ...acc, [item.item]: dataArr[idx] }
+        }, {})
+      }
+      return newData
+    }
+  },
+  KeyStatistics: {
+    name: 'defaultKeyStatistics',
+    handleData: data => {
+      return data?.quoteSummary.result.find(x => x).defaultKeyStatistics
+    }
+  },
+  IncomeStatement: {
+    name: 'incomeStatementHistory',
+    handleData: data => {
+      return data?.quoteSummary.result.find(x => x).incomeStatementHistory
+        .incomeStatementHistory
+    }
+  },
+  FinancialData: {
+    name: 'financialData',
+    handleData: data => {
+      return data?.quoteSummary.result.find(x => x).financialData
+    }
+  },
+  EarningsData: {
+    name: 'calendarEvents',
+    handleData: data => {
+      const earningsDate = data?.quoteSummary.result
+        .find(x => x)
+        .calendarEvents.earnings.earningsDate.find(x => x)
+      return earningsDate
+        ? earningsDate
+        : {
+            raw: 'N/A',
+            fmt: 'N/A'
+          }
+    }
+  },
+  Earnings: {
+    name: 'earnings',
+    handleData: data => {
+      return data?.quoteSummary.result.find(x => x).earnings.financialsChart
+        .yearly
+    }
+  },
+  CashflowStatement: {
+    name: 'cashflowStatementHistory',
+    handleData: data => {
+      return data?.quoteSummary.result.find(x => x).cashflowStatementHistory
+        .cashflowStatements
     }
   }
 }
