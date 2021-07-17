956e2facfc92a06648f18d53812e69f6dd50757a
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-07-17 16:01:57 +0800

    footer thicker, fine tune on highlight layout

--- components/Layout/Footer/AppContent.js
+++ components/Layout/Footer/AppContent.js
@@ -1,42 +1,17 @@
+import { defaultFooterNavElements } from '@/config/settings'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Badge from 'react-bootstrap/Badge'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
-import { BiTrendingUp } from 'react-icons/bi'
-import { GoCalendar } from 'react-icons/go'
-import { RiProfileLine, RiPagesLine } from 'react-icons/ri'
-
-const defaultNavElements = [
-  {
-    label: 'Profile',
-    href: '/profile',
-    icon: <RiProfileLine />
-  },
-  {
-    label: 'Main',
-    href: '/highlight',
-    icon: <RiPagesLine />
-  },
-  {
-    label: 'Trend',
-    href: '/trend',
-    icon: <BiTrendingUp />
-  },
-  {
-    label: 'Calendar',
-    href: '/calendar',
-    icon: <GoCalendar />
-  }
-]
 
 const AppContent = () => {
   const router = useRouter()
 
   return (
-    <Container className="overflow-auto">
-      {defaultNavElements.map((item, idx) => {
+    <Container className="overflow-auto my-2">
+      {defaultFooterNavElements.map((item, idx) => {
         const active = router.pathname === item.href
         return (
           <Col xs={'auto'} key={idx}>
