748e0572868512e6f4cc3e3440c67def88cab5ac
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-08-13 22:53:16 +0800

    enhance peers page

--- pages/api/moneycnn/getPeers.js
+++ pages/api/moneycnn/getPeers.js
@@ -1,42 +1,20 @@
-import { extractYahooInfo } from '@/config/peers'
-import { millify } from '@/lib/commonFunction'
+import Quote from '@/lib/class/quote'
 import { getPeers } from '@/lib/moneycnn/getPeers'
 import { getAPIResponse } from '@/lib/request'
-import { getQuote } from '@/lib/yahoo/getQuote'
-
-const getFormattedValue = (format, value) => {
-  return (format === 'millify' && millify(value)) || value
-}
 
 const getData = async args => {
   const { ticker } = args
   const data = await getPeers(ticker)
 
-  const quotes = await Promise.all(
+  const quotesValid = await Promise.all(
     data.map(async item => {
-      const quoteArr = await getQuote(item.Ticker)
-      return quoteArr.find(x => x) || {}
+      const quote = new Quote(item.Ticker)
+      await quote.request()
+      return quote.valid
     })
   )
 
-  const newData = [...data].map(item => {
-    const data = quotes.find(x => x?.symbol === item.Ticker)
-
-    const newItem = {
-      ...item,
-      ...extractYahooInfo.reduce((acc, cur) => {
-        const newAcc = {
-          ...acc,
-          [cur.label]: getFormattedValue(cur.format, (data || {})[cur.field])
-        }
-        return newAcc
-      }, {})
-    }
-
-    return newItem
-  })
-
-  return newData
+  return data.filter((_x, i) => quotesValid[i])
 }
 
 export default async (req, res) => {
