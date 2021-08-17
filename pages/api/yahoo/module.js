ee1e1bd245eb6d67526e0be5caad111bf642307d
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2021-08-17 19:57:39 +0800

    centralize getModuleData logic

--- pages/api/yahoo/module.js
+++ pages/api/yahoo/module.js
@@ -1,46 +1,10 @@
-import ModuleQuote from '@/lib/class/moduleQuote'
-import { getModules } from '@/lib/yahoo'
+import { getModulesData } from '@/lib/yahoo'
 
 export default async (req, res) => {
   const { ticker, name } = req.query
 
-  const response = {
-    result: null
-  }
-
-  const modules = getModules(name)
-
-  try {
-    const tickers = [...new Set([].concat(ticker))]
-
-    response.result = await Promise.all(
-      tickers.map(async t => await getQuoteModules(t, modules))
-    )
-
-    response.message = 'OK'
-  } catch (error) {
-    console.error(error)
-    response.message = error.message
-  }
+  const response = await getModulesData(ticker, name)
 
   res.statusCode = 200
   res.json(response)
-}
-
-const getQuoteModules = async (ticker, modules) => {
-  const quote = new ModuleQuote(ticker)
-
-  await quote.request()
-  const request = async m => await quote.requestModule(m)
-  await Promise.all(modules.map(request))
-
-  const quoteData = {
-    name: 'quote',
-    data: quote.valid ? { ...quote.quoteData } : undefined
-  }
-
-  return {
-    ...quote.moduleData,
-    modules: [...quote.moduleData.modules, quoteData]
-  }
 }
