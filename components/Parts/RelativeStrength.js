7b46bf90c8ddca9c28c12b2acbb2acbfe64dad6d
alexskt123 <alex.kt.so@pccw.com>
2022-03-19 23:11:44 +0800

    more library

--- components/Parts/RelativeStrength.js
+++ components/Parts/RelativeStrength.js
@@ -1,12 +1,13 @@
 import { Fragment, useState, useEffect } from 'react'
 
+import { Line } from 'react-chartjs-2'
+
 import { rsChartOptions } from '@/config/price'
 import { rsDays } from '@/config/rs'
 import { useStaticSWR } from '@/lib/request'
-import { Line } from 'react-chartjs-2'
 
 function RelativeStrength({ ticker, datePrice, inputDays }) {
-  const [chartData, setChartData] = useState({})
+  const [chartData, setChartData] = useState({ datasets: [] })
 
   const calRS = (inputDays, tickerData, spxData) => {
     const rsList = [...Array(parseInt(inputDays))].map((_x, i) => {
@@ -60,6 +61,7 @@ function RelativeStrength({ ticker, datePrice, inputDays }) {
 
   return (
     <Fragment>
+      aa
       <Line data={chartData} options={rsChartOptions} />
     </Fragment>
   )
